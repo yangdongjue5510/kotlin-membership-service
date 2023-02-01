855c6376b913a63a99be087f7e031b0f9899ce2c
yangdongjue5510 <djwhy5510@gmail.com>
2023-02-01 15:58:56 +0900

    feat: 프로젝트 생성

---
+++ src/main/kotlin/yangdongjue/membershipservice/MembershipServiceApplication.kt
@@ -0,0 +1,11 @@
+package yangdongjue.membershipservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MembershipServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<MembershipServiceApplication>(*args)
+}
---
+++ src/test/kotlin/yangdongjue/membershipservice/MembershipServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package yangdongjue.membershipservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MembershipServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
